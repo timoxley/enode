ec8f39205ef16a4502943ac0a130f225d31aff73
Tim Oxley <secoif@gmail.com>
2012-01-27 19:33:50 +1000

    Correct simple client/server example

--- examples/simple/client.js
+++ examples/simple/client.js
@@ -1,3 +1,15 @@
 var enode = require('../../index')
 
+var api = {
+  whoami: function(callback) {
+    callback(null, "client")
+  }
+}
 
+var client = new enode.client(api).connect(5000)
+
+client.on('ready', function(remote) {
+  remote.whoami(function(err, value) {
+    console.log('client connected to', value)
+  })
+})
--- examples/simple/server.js
+++ examples/simple/server.js
@@ -1,7 +1,17 @@
 var enode = require('../../index')
 
+var api = {
+  whoami: function(callback) {
+    console.log('server whoami')
+    callback(null, "server")
+  }
+}
 
-var Server = new enode.Server(5000, {
-  
+var server = new enode.server(api).listen(5000)
 
+server.on('connect', function(remote, connection) {
+  console.log('new connection', connection.id)
+  remote.whoami(function(err, value) {
+    console.log('server connected to', value)
+  })
 })
