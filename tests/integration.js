2e1353e1b8594830ae43087c400cf6ebfc8dfd4c
Tim Oxley <secoif@gmail.com>
2012-01-27 21:51:30 +1000

    wrap server/client api calls so they are also passed remote and connection

--- tests/integration.js
+++ tests/integration.js
@@ -31,43 +31,92 @@ describe('handing an API', function() {
   }
   beforeEach(shutdown)
   afterEach(shutdown)
-
-  it('client can call server methods', function(done) {
-    var api = {
-      status: "working",
-      whoami: function(callback) {
-        callback(null, "server")
+  describe('client', function() {
+    it('can call server methods', function(done) {
+      var api = {
+        status: "working",
+        whoami: function(callback) {
+          callback(null, "server")
+        }
       }
-    }
-    server = new Server(api).listen(PORT)
-    client = new Client().connect(PORT)
-    client.on('ready', function(remote) {
-      assert.equal(remote.status, "working")
-      remote.whoami(function(err, val) {
-        assert.equal(val, "server")
-        assert.ok(!err)
-        done()
+      server = new Server(api).listen(PORT)
+      client = new Client().connect(PORT)
+      client.on('ready', function(remote) {
+        assert.equal(remote.status, "working")
+        remote.whoami(function(err, val) {
+          assert.equal(val, "server")
+          assert.ok(!err)
+          done()
+        })
+      })
+    })
+    it('api is passed connection/remote to each call', function(done) {
+      var connectionId
+      var api = {
+        status: "working",
+        whoami: function(callback, remote, connection) {
+          assert.ok(remote)
+          assert.ok(connection)
+          assert.equal(connection.id, connectionId)
+          callback(null, connection)
+        }
+      }
+      server = new Server(api).listen(PORT)
+      server.on('connect', function(remote, connection) {
+        connectionId = connection.id
+      })
+      client = new Client().connect(PORT)
+      client.on('ready', function(remote, connection) {
+        remote.whoami(function(err, val) {
+          done()
+        })
       })
     })
   })
-  it('server can call client methods', function(done) {
-    var api = {
-      status: "working",
-      whoami: function(callback) {
-        callback(null, "client")
+  describe('server', function() {
+    it('can call client methods', function(done) {
+      var api = {
+        status: "working",
+        whoami: function(callback) {
+          callback(null, "client")
+        }
       }
-    }
-    server = new Server().listen(PORT)
-    server.on('connect', function(remote) {
-      assert.equal(remote.status, "working")
-      remote.whoami(function(err, val) {
-        assert.equal(val, "client")
-        assert.ok(!err)
-        done()
+      server = new Server().listen(PORT)
+      server.on('connect', function(remote) {
+        assert.equal(remote.status, "working")
+        remote.whoami(function(err, val) {
+          assert.equal(val, "client")
+          assert.ok(!err)
+          done()
+        })
+      })
+
+      client = new Client(api).connect(PORT)
+    })
+    it('api is passed connection/remote to each call', function(done) {
+      var connectionId
+      var api = {
+        status: "working",
+        whoami: function(callback, remote, connection) {
+          assert.ok(remote)
+          assert.ok(connection)
+          assert.equal(connection.id, connectionId)
+          callback(null, connection)
+        }
+      }
+      server = new Server().listen(PORT)
+      server.on('connect', function(remote, connection) {
+        remote.whoami(function(err, val) {
+          done()
+        })
+      })
+      client = new Client(api).connect(PORT)
+      client.on('ready', function(remote, connection) {
+        connectionId = connection.id
       })
     })
 
-    client = new Client(api).connect(PORT)
   })
+
 })
 
